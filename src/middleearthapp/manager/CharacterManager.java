a2a44c16d936848fd30cb80b2d93ae9d59805a1f
vdawson17 <vincegdawson@gmail.com>
2025-03-05 21:27:45 -0600

    Added comments and Javadoc to all methods in CharacterManager

--- src/middleearthapp/manager/CharacterManager.java
+++ src/middleearthapp/manager/CharacterManager.java
@@ -6,13 +6,17 @@ public class CharacterManager {
 	private MiddleEarthCharacter[] characters;
 	private int size;
 
+	/**
+	 * default constructor initializing default values for characters and size
+	 */
 	public CharacterManager() {
 		 this.characters = new MiddleEarthCharacter[10]; 
 	     this.size = 0;
 	}
+	
 	/**
-	 * @param characters 
-	 * @param size
+	 * @param characters Array of characters
+	 * @param size	number of characters in the array
 	 */
 	public CharacterManager(MiddleEarthCharacter[] characters, int size) {
 		super();
@@ -20,6 +24,11 @@ public class CharacterManager {
 		this.size = size;
 	}
 	
+	/**
+	 * addCharacter adds a new character, and doubles the size of the array if the array is full
+	 * @param c new character to be added
+	 * @return	Returns true if addCharacter function is successful
+	 */
 	public boolean addCharacter(MiddleEarthCharacter c) {
 		if (c == null) {
 			return false; //makes sure that empty object isn't passed in*/
@@ -32,49 +41,66 @@ public class CharacterManager {
 			characters = newCharacters;
 			
 		}
-		characters[size] = c; //size incremented to keep track of # of characters
-		size++;
+		characters[size] = c; //adds character to end of characters array
+		size++; //size incremented to keep track of # of characters
 		
 		System.out.println("Character added.");
-		return true;
-		//adds character to end of characters array
+		return true; //indicates success
+		
 	}
 	
-	
+	/**
+	 * Search for a character by name, return that character
+	 * @param name Name to be searched for within characters
+	 * @return Returns character object if name matches, returns null if no match is found
+	 */
 	public MiddleEarthCharacter getCharacter(String name) {
 		for(int i = 0; i < size; i++) { //iterates through characters until name matches given string
 			if (characters[i] != null && characters[i].getName().equals(name)) {
                 return characters[i]; //returns character object if name matches
             }
 		}
-			return null;
+			return null; //indicates failure to find character
 	}
 	
+	/**
+	 * Updates the character's attributes.
+	 * @param character The character that will be updated. 
+	 * @param name New name for the character.
+	 * @param health Updated health value.
+	 * @param power Updated power value.
+	 * @return true if update was made, false if not
+	 */
 	public boolean updateCharacter (MiddleEarthCharacter character, String name, int health, int power) {
-		boolean flag = false;
+		boolean flag = false; //flag to verify if character is updated or not 
 		
-		if (character == null) {
-			return flag;
+		if (character == null || name == null) {
+			return flag; //returns false if null character or name is given
 		}
 		
-		if (!character.getName().equals(name)) {
+		if (!character.getName().equals(name)) { //updates name given new name
 			character.setName(name);
 			flag = true;
 		}
 		
-		if (character.getHealth() != health) {
+		if (character.getHealth() != health) { //updates health given new health
 			character.setHealth(health);
 			flag = true;
 		}
 		
-		if (character.getPower() != power) {
+		if (character.getPower() != power) { //updates power given new power
 			character.setPower(power);
 			flag = true;
 		}
 		
-		return flag;
+		return flag; //return flag, will be false if no change made, true if change is made
 	}
 	
+	/**
+	 * Deletes a given character.
+	 * @param character Character to be deleted
+	 * @return false if character not deleted, true otherwise
+	 */
 	public boolean deleteCharacter(MiddleEarthCharacter character) {
 		if (character == null) {
 			return false; //false if no character is given
@@ -93,16 +119,18 @@ public class CharacterManager {
 				System.out.println("Removed.");
 				return true;}
 			}
-		return false;
+		return false; //return false if given character is not found
 	}
 	
 	
-	
+	/**
+	 * Displays all characters using displayInfo();
+	 */
 	public void displayAllCharacters() {
-		for(int i = 0; i<size; i++) {
+		for(int i = 0; i<size; i++) { //iterates through each character and prints out display of that character.
 			characters[i].displayInfo();
 		};
-	}
+	} 
 	
 	
 	
